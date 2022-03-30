18ea087fea061bf5046e5e2287f26c05bc4ce604
Cerebra Catalyst Team <cerebra-catalyst-team@google.com>
2022-03-30 13:13:23 -0700

    Internal change
    PiperOrigin-RevId: 438378693

--- aqt/jax_legacy/jax/imagenet/configs/experimental/resnet50_w8_a1_norelu.sh
+++ aqt/jax_legacy/jax/imagenet/configs/experimental/resnet50_w8_a1_norelu.sh
@@ -14,8 +14,10 @@
 # limitations under the License.
 
 gxm ../../google/xm_launch.py \
-  --platform=df --tpu_topology=8x8 \
+  --platform=df \
+  --tpu_topology=8x8 \
   --batch_size=8192 \
   --hparams_config_filename=experimental/resnet50_w8_a1_norelu.py \
   --name=resnet50-w8a1-baseline \
-  --xm_resource_pool=peace --xm_resource_alloc=user:peace/catalyst
+  --xm_resource_pool=peace \
+  --xm_resource_alloc=user:peace/catalyst
