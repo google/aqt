8eccc415ae3a0e451e14ee1dfe27cef28d34ac85
Cerebra Catalyst Team <cerebra-catalyst-team@google.com>
2023-02-13 14:29:32 -0800

    Test.
    PiperOrigin-RevId: 509334141

---
+++ aqt/jax/aqt_dot_general_research_test.py
@@ -0,0 +1,29 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for dot_general."""
+from absl.testing import absltest
+from absl.testing import parameterized
+import numpy as np
+
+
+class AqtDotGeneralResearchTest(parameterized.TestCase):
+
+  def test_empty(self):
+    t = np.random.normal(size=6).reshape((2, 3))
+    np.testing.assert_array_equal(t, t)
+
+
+if __name__ == "__main__":
+  absltest.main()
