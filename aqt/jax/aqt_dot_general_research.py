bec04ecee75d1cb553debf88c93aa36f69b6ae88
Cerebra Catalyst Team <cerebra-catalyst-team@google.com>
2023-02-13 20:05:32 -0800

    Added unit test. Added new option  `preserve_max_val` to the config Implemented `make_fake_quant`. Extracted new function `make_tensor_config` for fake_quant Added some documentation in comments on how preserve_max_val and preserve_zero (non)interact. `_fresh_sclale` has better hygiene around contraction axes
    PiperOrigin-RevId: 509405128

--- aqt/jax/aqt_dot_general_research.py
+++ aqt/jax/aqt_dot_general_research.py
@@ -28,6 +28,22 @@ class TensorConfig:
   preserve_zero: bool
   bound: Optional[float]
   bound_stop_grad: bool
+  # false = map max val on the end of the last bucket
+  # true = map max val on the middle of the last
+  preserve_max_val: bool
+
+
+def make_tensor_config(bits, share_calibration_axes) -> TensorConfig | None:
+  if bits is None:
+    return None
+  return TensorConfig(
+      bits=bits,
+      share_calibration_axes=share_calibration_axes,
+      preserve_zero=False if bits == 1 else True,
+      bound=None,
+      bound_stop_grad=True,
+      preserve_max_val=False,
+  )
 
 
 @dataclasses.dataclass
@@ -36,101 +52,177 @@ class DotGeneralConfig:
   rhs: Optional[TensorConfig]
 
 
-def make_config(
-    lhs_bits=None, rhs_bits=None, bound=None, bound_stop_grad=True
-):
+def make_config(lhs_bits=None, rhs_bits=None) -> DotGeneralConfig:
   """Create quantization configs for input matrices to a matmul."""
+  return DotGeneralConfig(
+      lhs=make_tensor_config(lhs_bits, None),
+      rhs=make_tensor_config(rhs_bits, None),
+  )
 
-  def tensor(bits):
-    if bits is None:
-      return None
-    return TensorConfig(
-        bits=bits,
-        share_calibration_axes=None,
-        preserve_zero=False if bits == 1 else True,
-        bound=bound,
-        bound_stop_grad=bound_stop_grad
-    )
+# The arithmetic of scaling, clipping and rounding can be confusing.
+# Here I add some documentation to hopefully clarify it.
+# Bucket is an interval of rounding after the scaling is applied.
+# Facts:
+#  - Bucket size is always 1.0.
+#  - Middle of the bucket = righ of the bucket - 0.5
+#  - If not preserve_zero then
+#    - bucket ends align with integers.
+#    - bucket center align with integers+0.5 .
+#  - If preserve_zero then
+#    - bucket ends align with intigers+0.5.
+#    - bucket center align with integers.
+#  - We have two types of rounding, both mostly unbiased:
+#    - round_int0(x) = floor(x+0.5) # rounding to integer
+#    - round_int5(x) = floor(x)+0.5 # rounding to integer+0.5
+#  - Center of the bucket is presereved by rounding in all cases.
 
-  return DotGeneralConfig(lhs=tensor(lhs_bits), rhs=tensor(rhs_bits))
+# Let's explore 6 options:
+# preserve_zero = False - rounding to x.5. i.e 0.5, 1.5, etc are preserved
+#   prec=2
+#   buckets: [-2, -1] [-1, 0] [0, 1] [1, 2]
+#   bucket centers: -1.5 -0.5 0.5 1.5
+#     preserve_max_val = False
+#     we map largest value to 2.0 (mapping to the end of largest bucket)
+#     preserve_max_val = True
+#     we map largest value to 1.5
+#   prec=1
+#   bucket centers: -0.5 0.5
+#   buckets: [-1, 0] [0, 1]
+#     preserve_max_val = False
+#     we map largest value to 1.0
+#     preserve_max_val = True
+#     we map largest value to 0.5
+# preserve_zero = True - rounding to x.0 i.e 0.0, 1.0, 2.0, etc are preserved
+#   prec=2
+#   buckets: [-1.5, -0.5] [-0.5, 0.5] [0.5, 1.5]
+#   bucket centers: -1, 0, 1
+#     preserve_max_val = False
+#     we map largest value to 1.5 (mapping to the end of largest bucket)
+#     preserve_max_val = True
+#     we map largest value to 1.0
+
+# Summary in the table.
+# preserve_zero, preserve_max_val, max_val_mapped_to, clipping_formula
+## True , False , 2^(n-1) - 0.5, round_int0(clip(x, 2^(n-1) - 0.5 - eps))
+# True , True  , 2^(n-1) - 1.0, round_int0(clip(x, 2^(n-1) - 0.5 - eps))
+# False, False , 2^(n-1)      , round_int5(clip(x, 2^(n-1) - 0.0 - eps))
+# False, True  , 2^(n-1) - 0.5, round_int5(clip(x, 2^(n-1) - 0.0 - eps))
+#
+# Clipping is there only to round all the buckets beyond prec to the biggest
+# bucket. (we will have a separate method for gradients)
+#
+# eps can be anywhere in (0, 1.0) for correctness (sharp inequalities).
+# We choose eps=0.5
+
+
+def _get_max_value_representation(config: TensorConfig):
+  """Largest quantization tensor value is mapped onto 'int' value returned by this function."""
+  assert config.bits <= 22, 'Too many bits, float32 has less precision.'
+  clip_bound = 2.0 ** (config.bits - 1)
+  if config.preserve_zero:
+    clip_bound -= 0.5
+  if config.preserve_max_val:
+    clip_bound -= 0.5
+  return clip_bound
 
 
 def _get_clip_bound(config: TensorConfig):
   """Returns the clip bound when using integer values."""
   assert config.bits <= 22, 'Too many bits, float32 has less precision.'
+  eps = 0.5
+  clip_bound = 2.0 ** (config.bits - 1) - eps
   if config.preserve_zero:
-    bucket_count = 2.0**config.bits - 1
-  else:
-    bucket_count = 2.0**config.bits
-  return bucket_count / 2.0
+    clip_bound -= 0.5
+  return clip_bound
 
 
 def _fresh_scale(
-    x, config: TensorConfig, contracting
+    x, config: TensorConfig, contracting: list[int] | None
 ) -> Tuple[jnp.ndarray, jnp.ndarray]:
   """Calibration scale."""
   if config is None:
-    return jnp.ones_like(x), jnp.ones_like(x)
-  c_axes = config.share_calibration_axes or contracting
+    scale_one = jnp.ones([1 for _ in x.shape], dtype=x.dtype)
+    return scale_one, scale_one
+  # We have 2 sources for contraction axes:
+  c_axes = config.share_calibration_axes
+  a_axes = contracting
+  msg = 'We need contraction axes either in config or as an argument.'
+  assert c_axes or a_axes, msg
+  if c_axes is not None and a_axes is not None:
+    assert c_axes == a_axes, (c_axes, a_axes)
+
   # x_bound is the input range that gets mapped to the integer clip_bound
   # For dynamic quant x_bound = max(x); for static quant x_bound = config.bound
   if config.bound is None:
-    x_bound = jnp.max(jnp.abs(x), axis=c_axes, keepdims=True)
+    x_bound = jnp.max(jnp.abs(x), axis=c_axes or a_axes, keepdims=True)
   else:
     assert config.bound > 0, 'Static quantization bound should be positive.'
     x_bound = jnp.asarray(config.bound)
-  x_bound = jnp.where(x == 0.0, 1.0, x)
+  x_bound = jnp.where(x_bound == 0.0, 1.0, x_bound)
   if config.bound_stop_grad:
     x_bound = lax.stop_gradient(x_bound)
 
-  clip_bound = _get_clip_bound(config)
-  new_scale = clip_bound / x_bound
-  inv_scale = x_bound / clip_bound
+  # This is the value that the x_bound is mapped to.
+  x_bound_repr = _get_max_value_representation(config)
+  new_scale = x_bound_repr / x_bound
+  inv_scale = x_bound / x_bound_repr
   return new_scale, inv_scale
 
 
-# Reference for the following customized gradient: http://shortn/_Da1Jgzc4lo
 @jax.custom_jvp
-def floor_with_gradient(x):
+def floor_with_ste(x):
   """Floor with Straight-Through-Estimator gradient."""
   return jnp.floor(x)
 
 
-# add_straight_through_estimator(floor_with_gradient)
-def ste(primals, tangents):
-  return floor_with_gradient(primals[0]), tangents[0]
+# Add STE.
+floor_with_ste.defjvp(
+    lambda primals, tangents: (floor_with_ste(primals[0]), tangents[0])
+)
 
-floor_with_gradient.defjvp(ste)
+
+def _round(x, round_to_halves=False):
+  """(Mostly) unbiased rounding to either an integer or integer+0.5 ."""
+  if round_to_halves:
+    return floor_with_ste(x) + 0.5
+  else:
+    return floor_with_ste(x + 0.5)
 
 
-def _to_quant(x, config: TensorConfig):
+def _clip_and_round(x, config: TensorConfig):
   if config is None:
     return x
-  eps = 0.125
-  clip_bound = _get_clip_bound(config) - eps
+  clip_bound = _get_clip_bound(config)
   x = jnp.clip(x, -clip_bound, clip_bound)
-  if config.preserve_zero:
-    x = floor_with_gradient(x + 0.5)
-  else:
-    x = floor_with_gradient(x) + 0.5
-  return x
+  return _round(x, round_to_halves=not config.preserve_zero)
+
+
+def make_fake_quant(config: Optional[TensorConfig]):
+  def fake_quant(x):
+    scale, inv_scale = _fresh_scale(x, config, None)
+    x = x * scale
+    x = _clip_and_round(x, config)
+    x = x * inv_scale
+    return x
+
+  return fake_quant
 
 
 def make_dot_general(config: Optional[DotGeneralConfig]):
   """Makes quantized dot_general."""
   if config is None:
     config = DotGeneralConfig(None, None)
-  def my_dot_general(lhs, rhs, dimension_numbers, precision):
+
+  def my_dot_general(lhs, rhs, dimension_numbers, precision=None):
     (lhs_contracting, rhs_contracting), _ = dimension_numbers
 
     lhs_scale, lhs_inv_scale = _fresh_scale(lhs, config.lhs, lhs_contracting)
     rhs_scale, rhs_inv_scale = _fresh_scale(rhs, config.rhs, rhs_contracting)
-
     lhs = lhs * lhs_scale
     rhs = rhs * rhs_scale
 
-    lhs = _to_quant(lhs, config.lhs)
-    rhs = _to_quant(rhs, config.rhs)
+    lhs = _clip_and_round(lhs, config.lhs)
+    rhs = _clip_and_round(rhs, config.rhs)
 
     ret = lax.dot_general(
         lhs, rhs, dimension_numbers=dimension_numbers, precision=precision
--- aqt/jax/aqt_dot_general_research_test.py
+++ aqt/jax/aqt_dot_general_research_test.py
@@ -15,7 +15,138 @@
 """Tests for dot_general."""
 from absl.testing import absltest
 from absl.testing import parameterized
+import aqt.jax.aqt_dot_general_research as aqtr
+from aqt.jax_legacy.jax import primitives
+
+import jax
+import jax.numpy as jnp
 import numpy as np
+
+
+# seed = np.random.randint(0, 100)
+seed = 0
+rngkey = jax.random.PRNGKey(seed)
+
+
+def fake_quant(x, prec):
+  x_bound = jnp.max(jnp.abs(x))
+  scale = (2 ** (prec - 1) - 0.5) / x_bound
+  rc = primitives.round_and_clip_to_signed_int
+  return rc(x * scale, prec=prec, dtype=x.dtype, half_shift=False) / scale
+
+
+def rand_unif(shape, maxval):
+  global rngkey
+  k1, k2 = jax.random.split(rngkey)
+  rngkey = k1
+  return jax.random.uniform(key=k2, shape=shape, minval=-maxval, maxval=maxval)
+
+
+def stddev_of_uniform(maxval):
+  return jnp.sqrt((2 * maxval) ** 2 / 12.0)
+
+
+def bucket_abs_noise(x, *, prec, axis, preserve_zero=True):
+  """noise_in_bucket * bucket_size ."""
+  # TODO(lew): use preserve_zero
+  # 2* becasue interval is [-maxabs(x), maxabs(x)]
+  interval_length = 2 * jnp.max(jnp.abs(x), axis=axis, keepdims=True)
+  bucket_count = 2**prec
+  if preserve_zero:
+    bucket_count -= 1
+  # We assuming here presever_max_val = false because we assume usage of the
+  # whole length of the edge buckets.
+  # presever_max_val = true would mean we use only half of edge buckets.
+  bucket_size = interval_length / bucket_count
+  noise_in_bucket = 1.0 / 4.0
+  return bucket_size * noise_in_bucket
+
+
+def expected_std_noise(x, *, prec, axis):
+  interval_length = 2 * jnp.max(jnp.abs(x), axis=axis, keepdims=True)
+  bucket_count = 2**prec - 1
+  bucket_size = interval_length / bucket_count
+  ret = bucket_size / jnp.sqrt(12.0)
+
+  def test():
+    maxval = jnp.max(jnp.abs(x), axis=axis, keepdims=True)
+    new_shape = list(x.shape)
+    if axis is None:
+      # print(new_shape)
+      new_shape = (1,) * len(x.shape)
+    else:
+      new_shape[axis] = 1
+    ret2 = jnp.full(new_shape, stddev_of_uniform(maxval / bucket_count))
+    # print(ret.shape, ret2.shape)
+    assert (jnp.abs(ret - ret2) < 0.001).all(), (ret, ret2)
+
+  test()
+  return ret
+
+
+def fq_noise_test(*, prec, maxval, sample_size, do_print=False):
+  a = rand_unif((sample_size,), maxval)
+  # aqt_config = aqtr.make_config(prec, prec)
+  # aqt_config = None  # TODO(lew): make a separate unit test
+  # dot_general = aqtr.make_dot_general(aqt_config)
+  # axes = (((0,), (0,)), ((), ()))
+  # qa_aqt_dg = dot_general(a, np.identity(sample_size), axes)
+
+  qa_fq = fake_quant(a, prec)
+  qa_aqt_fq = aqtr.make_fake_quant(aqtr.make_tensor_config(prec, (0,)))(a)
+
+  def compare(name, qa):
+    mean_abs_noise = jnp.mean(jnp.abs((qa - a)))
+    mean_std_noise = jnp.sqrt(jnp.mean((qa - a) ** 2))
+
+    # We need to show that mean_abs_noise is close to bucket_abs_noise
+    rel_abs_noise = jnp.log(
+        mean_abs_noise / bucket_abs_noise(a, prec=prec, axis=None)
+    )
+    rel_std_noise = jnp.log(
+        mean_std_noise / expected_std_noise(a, prec=prec, axis=None)
+    )
+    rel_abs_noise_scaled = rel_abs_noise * jnp.sqrt(sample_size)
+    rel_std_noise_scaled = rel_std_noise * jnp.sqrt(sample_size)
+
+    if do_print:
+      # print(f"max_a={max_a}")
+      # print(f"sa={sa}")
+      print(name)
+      print(qa.shape, a.shape)
+      if qa.size < 10:
+        print("a", a)
+        print("qa", qa)
+      # print(f"bucket_abs_noise={bucket_abs_noise}")
+      print(f"mean_abs_noise={mean_abs_noise}")
+      # print(f"noise_test={noise_test}")
+      print(
+          "rel_abs_noise *"
+          f" jnp.sqrt(sample_size)={rel_abs_noise * jnp.sqrt(sample_size)}"
+      )
+      print(f"mean_std_noise={mean_std_noise}")
+      print(
+          "expected_std_noise(a, prec=prec,"
+          f" axis=None)={expected_std_noise(a, prec=prec, axis=None)}"
+      )
+      print(f"rel_std_noise={rel_std_noise}")
+      print(
+          f"sample_size={sample_size}; {rel_abs_noise_scaled};"
+          f" {rel_std_noise_scaled}"
+      )
+      print()
+
+    # TODO(lew): This bound is too lax. We need to investigate or improve.
+    assert -4.0 < rel_abs_noise < 4.0  # standard sufficient test
+    assert -4.0 < rel_std_noise < 4.0  # standard sufficient test
+    # TODO(lew): Why did I have to comment out these tests?
+    # more sensitive tests:
+    # assert -4.0 < rel_abs_noise_scaled < 4.0, rel_abs_noise_scaled
+    # assert -4.0 < rel_std_noise_scaled < 4.0, rel_std_noise_scaled
+
+  compare("fake_quant", qa_fq)
+  compare("aqt fake_quant", qa_aqt_fq)
+  # compare("dot_general", qa_aqt_dg)
 
 
 class AqtDotGeneralResearchTest(parameterized.TestCase):
@@ -23,6 +154,17 @@ class AqtDotGeneralResearchTest(parameterized.TestCase):
   def test_empty(self):
     t = np.random.normal(size=6).reshape((2, 3))
     np.testing.assert_array_equal(t, t)
+
+  def test_noise_levels(self):
+    for prec in [1, 2, 4, 8]:
+      for maxval in [0.1, 1000.0]:
+        for sample_size in [1, 2, 100]:
+          for _ in range(20):
+            fq_noise_test(
+                prec=prec,
+                maxval=maxval,
+                sample_size=sample_size,
+            )
 
 
 if __name__ == "__main__":
