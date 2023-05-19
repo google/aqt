d75ccce357c3bb8e988854b4e23e692f42cfc2f6
Rebecca Chen <rechen@google.com>
2023-05-19 14:05:20 -0700

    Internal change.
    PiperOrigin-RevId: 533546540

--- aqt/jax_legacy/utils/pandas_utils.py
+++ aqt/jax_legacy/utils/pandas_utils.py
@@ -265,7 +265,7 @@ def apply_filter_drop_rename_operations(
     df = df.sort_values(by=by, ascending=ascending)
 
   df = df.reset_index(drop=True)
-  return df
+  return df  # pytype: disable=bad-return-type  # typed-pandas
 
 
 def boxplot_with_group_by_sorted_by_median(df: pd.DataFrame,
