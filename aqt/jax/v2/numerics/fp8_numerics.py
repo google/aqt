ddafdc100fb0eef907dc360c94688dc4a5557fab
Cerebra Catalyst Team <cerebra-catalyst-team@google.com>
2024-01-04 06:23:32 -0800

    Add fp8_numerics support
    PiperOrigin-RevId: 595686500

---
+++ aqt/jax/v2/numerics/fp8_numerics.py
@@ -0,0 +1,130 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Numerics for fp8."""
+
+from typing import Any, Optional
+from aqt.jax.v2 import calibration
+from aqt.jax.v2 import config
+from aqt.jax.v2 import stochastic_rounding
+from aqt.jax.v2.flax import aqt_flax
+from aqt.jax.v2.numerics import numerics
+import flax.struct
+import jax
+import jax.numpy as jnp
+
+
+FP8_DTYPE = {'e4m3': jnp.float8_e4m3fn, 'e5m2': jnp.float8_e5m2}
+
+
+class Fp8Numerics(numerics.AqtNumerics, flax.struct.PyTreeNode):
+  """Numerics for fp8."""
+
+  dtype: Any
+  exponent_bits: int = 4
+  mantissa_bits: int = 3
+  noise_fn: Optional[stochastic_rounding.NoiseFn] = None
+
+  def _get_edge_of_last_fp8_bucket(self):
+    return jnp.finfo(self.dtype).max.astype(jnp.bfloat16)
+
+  def get_dtype(self):
+    return self.dtype
+
+  def fwd(self, x, context):
+    if not (
+        (self.exponent_bits == 4 and self.mantissa_bits == 3)
+        or (self.exponent_bits == 5 and self.mantissa_bits == 2)
+    ):
+      raise ValueError(
+          '(exponent_bits, mantissa_bits) can only be (4,3) or (5,2) but was '
+          f'({self.exponent_bits}, {self.mantissa_bits})'
+      )
+
+    if self.noise_fn is not None:
+      x = (x + self.noise_fn(x.shape, context.key)).astype(x.dtype)
+
+    # clip
+    fwd_clip_bound = self._get_edge_of_last_fp8_bucket()
+    x = jnp.clip(x, -1 * fwd_clip_bound, fwd_clip_bound)
+
+    # round
+    x = round_to_nearest_even(x, self.dtype)
+
+    return x
+
+  def abs_val_mapped_to(self):
+    return self._get_edge_of_last_fp8_bucket()
+
+  def vjp_fwd(self, x, context):
+    res = (x,)
+    return self.fwd(x, context), res
+
+  def vjp_bwd(self, res, grad):
+    # This is gradient of clip.
+    # For boundary values we will have full gradient.
+    # We might use something like this for calibrations other than abs(max(x))
+    # (x,) = res
+    # ret = (x <= edge_of_last_bucket) * (x >= -edge_of_last_bucket) * grad
+    del res
+    ret = grad
+    return (ret, None)
+
+
+def tensor_make_fp8(
+    exponent_bits: int = 4, mantissa_bits: int = 3
+) -> config.Tensor:
+  """Makes config.Tensor."""
+  # TODO(lew): refactor this into set_numerics()
+  return config.Tensor(
+      numerics=Fp8Numerics(
+          exponent_bits=exponent_bits,
+          mantissa_bits=mantissa_bits,
+          dtype=FP8_DTYPE[f'e{exponent_bits}m{mantissa_bits}'],
+          noise_fn=None,
+      ),
+      calib_shared_axes=None,
+      scale_stop_grad=True,
+      calibration=calibration.AbsMaxCalibration(),
+      po2_scale=False,
+      use_fake_quant=False,
+      use_fwd_quant=None,
+      preprocess=None,
+      context=config.Context(key=None, train_step=None),
+  )
+
+
+def round_to_nearest_even(x: jnp.ndarray, dtype: jnp.dtype) -> jnp.ndarray:
+  original_dtype = x.dtype
+  x = x.astype(dtype)
+  # bitcast_convert to uint8 to avoid allow_excess_precision set in XLA
+  x = jax.lax.bitcast_convert_type(x, jnp.uint8)
+  x = jax.lax.bitcast_convert_type(x, dtype)
+  return x.astype(original_dtype)
+
+
+def config_fwd_fp8(fwd_bits: str = 'e4m3') -> config.DotGeneral:
+  """Configs for FP8 forward pass."""
+  assert fwd_bits in FP8_DTYPE.keys(), 'FP8 only supports 4 or 5 exponent bits'
+  exponent_bits, mantissa_bits = int(fwd_bits[1]), int(fwd_bits[3])
+  cfg = aqt_flax.config_v4(fwd_bits=8, dlhs_bits=None, drhs_bits=None)
+  fp8_numerics = Fp8Numerics(
+      exponent_bits=exponent_bits,
+      mantissa_bits=mantissa_bits,
+      dtype=FP8_DTYPE[fwd_bits],
+      noise_fn=None,
+  )
+  config.set_fwd_numerics(cfg, fp8_numerics)
+  config.set_accumulator_dtype(cfg, jnp.float32, None, None)
+  config.set_stochastic_rounding(cfg, False, False, 'jax.uniform')
+  return cfg
---
+++ aqt/jax/v2/numerics/fp8_numerics_test.py
@@ -0,0 +1,157 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from absl.testing import parameterized
+from aqt.jax.v2.examples import flax_e2e_model
+import aqt.jax.v2.numerics.fp8_numerics as numerics
+import jax
+import jax.numpy as jnp
+import numpy as np
+from google3.testing.pybase import googletest
+
+
+class MyTest(parameterized.TestCase):
+
+  def test_2bit_mantissa(self):
+    x = jnp.array(
+        [
+            [0, 1, -1, 0.1234567, 100, 0.5, 0.00000000001, 0.01],
+            [
+                -2.9098532,
+                -0.8508397,
+                -0.14903745,
+                -3.572757,
+                0.63277215,
+                0.7478278,
+                1.3707844,
+                1.3319879,
+            ],
+        ],
+        dtype=jnp.float32,
+    )
+
+    result = numerics.round_to_nearest_even(x, jnp.float8_e5m2)
+
+    np.testing.assert_array_equal(
+        result,
+        np.array(
+            [
+                [0, 1, -1, 0.125, 96, 0.5, 0, 10 / 1024],
+                [-3, -0.875, -0.15625, -3.5, 0.625, 0.75, 1.25, 1.25],
+            ],
+            dtype=jnp.float32,
+        ),
+    )
+
+  def test_3bit_mantissa(self):
+    x = jnp.array(
+        [
+            [0, 1, -1, 0.1234567, 100, 0.5, 0.00000000001, 0.01],
+            [
+                -2.9098532,
+                -0.8508397,
+                -0.14903745,
+                -3.572757,
+                0.63277215,
+                0.7478278,
+                1.3707844,
+                1.3319879,
+            ],
+        ],
+        dtype=jnp.float32,
+    )
+
+    result = numerics.round_to_nearest_even(x, jnp.float8_e4m3fn)
+
+    np.testing.assert_array_equal(
+        result,
+        np.array(
+            [
+                [0, 1, -1, 0.125, 96, 0.5, 0, 10 / 1024],
+                [-3, -0.875, -0.15625, -3.5, 0.625, 0.75, 1.375, 1.375],
+            ],
+            dtype=jnp.float32,
+        ),
+    )
+
+  def test_retains_dtype(self):
+    result_f16 = numerics.round_to_nearest_even(
+        jnp.array([1], dtype=jnp.bfloat16), jnp.float8_e5m2
+    )
+    result_f32 = numerics.round_to_nearest_even(
+        jnp.array([1], dtype=jnp.float32), jnp.float8_e4m3fn
+    )
+
+    self.assertEqual(result_f32.dtype, jnp.float32)
+    self.assertEqual(result_f16.dtype, jnp.bfloat16)
+
+  @parameterized.parameters([
+      dict(fwd_bits="e4m3"),
+      dict(fwd_bits="e5m2"),
+  ])
+  def test_mnist_training(self, fwd_bits: str):
+    target_loss = {
+        "e4m3": {
+            "cpu": [
+                # One of them is: milan, rome, haswell
+                # Other is: skylake, cascadelake
+                4.084124088287353515625000000000,
+                4.084124565124511718750000000000,
+            ],
+            "TPU v2": [4.093867301940917968750000000000],
+            "TPU v3": [4.093903541564941406250000000000],
+            "TPU v4": [4.096024513244628906250000000000],
+            "TPU v5 lite": [4.093614101409912109375000000000],
+        },
+        "e5m2": {
+            "cpu": [
+                4.148341178894042968750000000000,
+            ],
+            "TPU v2": [4.117388725280761718750000000000],
+            "TPU v3": [4.117387771606445312500000000000],
+            "TPU v4": [4.134133338928222656250000000000],
+            "TPU v5 lite": [4.117388725280761718750000000000],
+        },
+    }
+
+    # RNGs
+    rng = jax.random.key(0)
+    rng, init_rng = jax.random.split(rng)
+    rng, ds_rng = jax.random.split(rng)
+    rng, input_rng = jax.random.split(rng)
+    del rng
+
+    # Dataset
+    ds_size = 8
+    ds = {
+        "image": jax.random.uniform(key=ds_rng, shape=(ds_size, 28, 28, 1)),
+        "label": jax.random.randint(
+            key=ds_rng, shape=(ds_size,), minval=0, maxval=10
+        ),
+    }
+
+    aqt_cfg = numerics.config_fwd_fp8(fwd_bits)
+    state = flax_e2e_model.create_train_state(init_rng, aqt_cfg)
+
+    _, train_loss, _ = flax_e2e_model.train_epoch(
+        state, ds, batch_size=ds_size // 2, rng=input_rng
+    )
+
+    device = jax.devices()[0].device_kind
+    if train_loss not in target_loss[fwd_bits][device]:
+      msg = f"train_loss={train_loss:.30f}, {device=}, {fwd_bits=}"
+      self.fail(msg)
+
+
+if __name__ == "__main__":
+  googletest.main()
