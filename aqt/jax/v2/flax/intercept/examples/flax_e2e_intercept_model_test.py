561d7b45a2952b8710970d6bb5b21f9ca2dda1ff
Blake Hechtman <blakehechtman@google.com>
2024-05-17 11:21:50 -0700

    [XLA:SPACE_TO_BATCH] correctly propagate on dot
    PiperOrigin-RevId: 634835520

--- aqt/jax/v2/flax/intercept/examples/flax_e2e_intercept_model_test.py
+++ aqt/jax/v2/flax/intercept/examples/flax_e2e_intercept_model_test.py
@@ -68,7 +68,7 @@ class MnistTest(parameterized.TestCase):
     )
     target_loss = {
         8: {
-            "TPU v5 lite": [3.222228527069091796875000000000],
+            "TPU v5 lite": [3.222529888153076171875000000000],
         },
         4: {
             "TPU v5 lite": [2.292296886444091796875000000000],
